8baf2f2f14cbe37799f063219a39ff7999d4538b
sinar <srafizad@ucdavis.edu>
2019-05-09 21:25:01 +0000

    practice 1

--- sss/server.R
+++ sss/server.R
@@ -8,18 +8,21 @@
 #
 
 library(shiny)
+library(ggplot2)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
    
-  output$distPlot <- renderPlot({
+  output$violinPlot <- renderPlot({
     
     # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+   # x    <- faithful[, 2] 
+   # bins <- seq(min(x), max(x), length.out = input$bins + 1)
     
     # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    #hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  ggplot(data=iris, aes_string(x="Species", y=input$trait, fill="Species")) +
+           geom_violin()
     
   })
   
